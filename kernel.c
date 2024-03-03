496b5a6b69513766e72875171d1d0f26be3b43d8
RyoAtWSL_LapTop <drsgon.work.36@gmail.com>
2024-03-03 11:26:19 +0900

    :bug: 不要な括弧を削除

--- kernel.c
+++ kernel.c
@@ -5,26 +5,25 @@ typedef uint32_t size_t;
 extern char __bss[], __bss_end[], __stack_top[];
 
 void *memset(void *buf, char c, size_t n) {
-    uint8_t *p = (uint8_t *)buf;
-    while (n--) {
+    uint8_t *p = (uint8_t *) buf;
+    while (n--)
         *p++ = c;
-    }
     return buf;
 }
 
 void kernel_main(void) {
-    memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
-    for (;;)
-        ;
+    for (;;);
 }
 
-__atribute__((section(".text.boot")))
-__atribute__((naked))
+__attribute__((section(".text.boot")))
+__attribute__((naked))
 void boot(void) {
     __asm__ __volatile__(
         "mv sp, %[stack_top]\n"
         "j kernel_main\n"
         :
-        : [stack_top] "r"(__stack_top));
+        : [stack_top] "r" (__stack_top)
+    );
 }
